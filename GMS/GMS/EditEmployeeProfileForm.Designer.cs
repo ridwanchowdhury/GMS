fb1e841100038c7205e6192bf38c3cd9f8d80ccc
ridwanchowdhury <ridroxx@gmail.com>
2022-04-09 01:51:27 +0600

    Add files via upload

---
+++ GMS/GMS/EditEmployeeProfileForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace GMS
+{
+    partial class EditEmployeeProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditEmployeeProfileForm));
+            this.updateEmployeeEnterBtn = new System.Windows.Forms.Button();
+            this.enterEmployeeTitleLabel = new System.Windows.Forms.Label();
+            this.updateEmployeeAddressTextBox = new System.Windows.Forms.TextBox();
+            this.updateEmployeeAddressLabel = new System.Windows.Forms.Label();
+            this.updateEmployeePhoneTextBox = new System.Windows.Forms.TextBox();
+            this.updateEmployeePhoneLabel = new System.Windows.Forms.Label();
+            this.updateEmployeeEmailTextBox = new System.Windows.Forms.TextBox();
+            this.updaeEmployeeEmailLabel = new System.Windows.Forms.Label();
+            this.updateEmployeeNameTextBox = new System.Windows.Forms.TextBox();
+            this.updateEmployeeNameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.updateEmployeePasswordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // updateEmployeeEnterBtn
+            // 
+            this.updateEmployeeEnterBtn.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeeEnterBtn.Location = new System.Drawing.Point(811, 316);
+            this.updateEmployeeEnterBtn.Name = "updateEmployeeEnterBtn";
+            this.updateEmployeeEnterBtn.Size = new System.Drawing.Size(168, 99);
+            this.updateEmployeeEnterBtn.TabIndex = 43;
+            this.updateEmployeeEnterBtn.Text = "Update";
+            this.updateEmployeeEnterBtn.UseVisualStyleBackColor = true;
+            this.updateEmployeeEnterBtn.Click += new System.EventHandler(this.updateEmployeeEnterBtn_Click);
+            // 
+            // enterEmployeeTitleLabel
+            // 
+            this.enterEmployeeTitleLabel.AutoSize = true;
+            this.enterEmployeeTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.enterEmployeeTitleLabel.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.enterEmployeeTitleLabel.Location = new System.Drawing.Point(91, 35);
+            this.enterEmployeeTitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.enterEmployeeTitleLabel.Name = "enterEmployeeTitleLabel";
+            this.enterEmployeeTitleLabel.Size = new System.Drawing.Size(548, 59);
+            this.enterEmployeeTitleLabel.TabIndex = 42;
+            this.enterEmployeeTitleLabel.Text = "Enter the following details : ";
+            // 
+            // updateEmployeeAddressTextBox
+            // 
+            this.updateEmployeeAddressTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeeAddressTextBox.Location = new System.Drawing.Point(203, 454);
+            this.updateEmployeeAddressTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.updateEmployeeAddressTextBox.Name = "updateEmployeeAddressTextBox";
+            this.updateEmployeeAddressTextBox.Size = new System.Drawing.Size(513, 34);
+            this.updateEmployeeAddressTextBox.TabIndex = 41;
+            // 
+            // updateEmployeeAddressLabel
+            // 
+            this.updateEmployeeAddressLabel.AutoSize = true;
+            this.updateEmployeeAddressLabel.BackColor = System.Drawing.Color.Transparent;
+            this.updateEmployeeAddressLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeeAddressLabel.Location = new System.Drawing.Point(91, 454);
+            this.updateEmployeeAddressLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.updateEmployeeAddressLabel.Name = "updateEmployeeAddressLabel";
+            this.updateEmployeeAddressLabel.Size = new System.Drawing.Size(96, 28);
+            this.updateEmployeeAddressLabel.TabIndex = 40;
+            this.updateEmployeeAddressLabel.Text = "Address : ";
+            // 
+            // updateEmployeePhoneTextBox
+            // 
+            this.updateEmployeePhoneTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeePhoneTextBox.Location = new System.Drawing.Point(203, 384);
+            this.updateEmployeePhoneTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.updateEmployeePhoneTextBox.Name = "updateEmployeePhoneTextBox";
+            this.updateEmployeePhoneTextBox.Size = new System.Drawing.Size(513, 34);
+            this.updateEmployeePhoneTextBox.TabIndex = 39;
+            // 
+            // updateEmployeePhoneLabel
+            // 
+            this.updateEmployeePhoneLabel.AutoSize = true;
+            this.updateEmployeePhoneLabel.BackColor = System.Drawing.Color.Transparent;
+            this.updateEmployeePhoneLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeePhoneLabel.Location = new System.Drawing.Point(91, 387);
+            this.updateEmployeePhoneLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.updateEmployeePhoneLabel.Name = "updateEmployeePhoneLabel";
+            this.updateEmployeePhoneLabel.Size = new System.Drawing.Size(81, 28);
+            this.updateEmployeePhoneLabel.TabIndex = 38;
+            this.updateEmployeePhoneLabel.Text = "Phone : ";
+            // 
+            // updateEmployeeEmailTextBox
+            // 
+            this.updateEmployeeEmailTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeeEmailTextBox.Location = new System.Drawing.Point(203, 328);
+            this.updateEmployeeEmailTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.updateEmployeeEmailTextBox.Name = "updateEmployeeEmailTextBox";
+            this.updateEmployeeEmailTextBox.Size = new System.Drawing.Size(513, 34);
+            this.updateEmployeeEmailTextBox.TabIndex = 37;
+           // this.updateEmployeeEmailTextBox.TextChanged += new System.EventHandler(this.updateEmployeeEmailTextBox_TextChanged);
+            // 
+            // updaeEmployeeEmailLabel
+            // 
+            this.updaeEmployeeEmailLabel.AutoSize = true;
+            this.updaeEmployeeEmailLabel.BackColor = System.Drawing.Color.Transparent;
+            this.updaeEmployeeEmailLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updaeEmployeeEmailLabel.Location = new System.Drawing.Point(91, 328);
+            this.updaeEmployeeEmailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.updaeEmployeeEmailLabel.Name = "updaeEmployeeEmailLabel";
+            this.updaeEmployeeEmailLabel.Size = new System.Drawing.Size(73, 28);
+            this.updaeEmployeeEmailLabel.TabIndex = 36;
+            this.updaeEmployeeEmailLabel.Text = "Email : ";
+            // 
+            // updateEmployeeNameTextBox
+            // 
+            this.updateEmployeeNameTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeeNameTextBox.Location = new System.Drawing.Point(203, 122);
+            this.updateEmployeeNameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.updateEmployeeNameTextBox.Name = "updateEmployeeNameTextBox";
+            this.updateEmployeeNameTextBox.Size = new System.Drawing.Size(513, 34);
+            this.updateEmployeeNameTextBox.TabIndex = 33;
+            // 
+            // updateEmployeeNameLabel
+            // 
+            this.updateEmployeeNameLabel.AutoSize = true;
+            this.updateEmployeeNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.updateEmployeeNameLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeeNameLabel.Location = new System.Drawing.Point(92, 122);
+            this.updateEmployeeNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.updateEmployeeNameLabel.Name = "updateEmployeeNameLabel";
+            this.updateEmployeeNameLabel.Size = new System.Drawing.Size(78, 28);
+            this.updateEmployeeNameLabel.TabIndex = 32;
+            this.updateEmployeeNameLabel.Text = "Name : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(94, 195);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 28);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Password : ";
+            // 
+            // updateEmployeePasswordTextBox
+            // 
+            this.updateEmployeePasswordTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateEmployeePasswordTextBox.Location = new System.Drawing.Point(203, 195);
+            this.updateEmployeePasswordTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.updateEmployeePasswordTextBox.Name = "updateEmployeePasswordTextBox";
+            this.updateEmployeePasswordTextBox.Size = new System.Drawing.Size(513, 34);
+            this.updateEmployeePasswordTextBox.TabIndex = 45;
+            // 
+            // EditEmployeeProfileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1067, 584);
+            this.Controls.Add(this.updateEmployeePasswordTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.updateEmployeeEnterBtn);
+            this.Controls.Add(this.enterEmployeeTitleLabel);
+            this.Controls.Add(this.updateEmployeeAddressTextBox);
+            this.Controls.Add(this.updateEmployeeAddressLabel);
+            this.Controls.Add(this.updateEmployeePhoneTextBox);
+            this.Controls.Add(this.updateEmployeePhoneLabel);
+            this.Controls.Add(this.updateEmployeeEmailTextBox);
+            this.Controls.Add(this.updaeEmployeeEmailLabel);
+            this.Controls.Add(this.updateEmployeeNameTextBox);
+            this.Controls.Add(this.updateEmployeeNameLabel);
+            this.Name = "EditEmployeeProfileForm";
+            this.Text = "EditEmployeeProfileForm";
+            //this.Load += new System.EventHandler(this.EditEmployeeProfileForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updateEmployeeEnterBtn;
+        private System.Windows.Forms.Label enterEmployeeTitleLabel;
+        private System.Windows.Forms.TextBox updateEmployeeAddressTextBox;
+        private System.Windows.Forms.Label updateEmployeeAddressLabel;
+        private System.Windows.Forms.TextBox updateEmployeePhoneTextBox;
+        private System.Windows.Forms.Label updateEmployeePhoneLabel;
+        private System.Windows.Forms.TextBox updateEmployeeEmailTextBox;
+        private System.Windows.Forms.Label updaeEmployeeEmailLabel;
+        private System.Windows.Forms.TextBox updateEmployeeNameTextBox;
+        private System.Windows.Forms.Label updateEmployeeNameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox updateEmployeePasswordTextBox;
+    }
+}
